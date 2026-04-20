2409014d84b0dda930f26bd5d86c4c1a176a2bdf
Douglas Javieri <douvan.22@gmail.com>
2026-04-19 22:03:53 -0400

    chore: estructura monorepo con pom padre, back-office-api y back-office-web

---
+++ back-office-api/src/main/java/com/fisioterapiakinevid/kinevid/KinevidApplication.java
@@ -0,0 +1,13 @@
+package com.fisioterapiakinevid.kinevid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinevidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinevidApplication.class, args);
+    }
+
+}
---
+++ back-office-api/src/test/java/com/fisioterapiakinevid/kinevid/KinevidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisioterapiakinevid.kinevid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinevidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
