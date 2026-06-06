6a6e08854b35fc52fe43bcdc97b2f6220b815ede
Douglas Javieri <douvan.22@gmail.com>
2026-06-06 07:47:54 -0400

    Ajustes del home

--- back-office-web/src/main/web/src/app/features/pages/home/home.component.ts
+++ back-office-web/src/main/web/src/app/features/pages/home/home.component.ts
@@ -1,8 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'knv-home-page',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit, OnDestroy {
+  images: string[] = [
+    'assets/images/foto1.jpg',
+    'assets/images/foto2.jpg',
+    'assets/images/foto3.jpg',
+    'assets/images/foto4.jpg',
+    'assets/images/foto5.jpg'
+  ];
+
+  currentIndex = 0;
+  private intervalId: any;
+
+  ngOnInit(): void {
+    this.startCarousel();
+  }
+
+  ngOnDestroy(): void {
+    this.stopCarousel();
+  }
+
+  startCarousel(): void {
+    this.intervalId = setInterval(() => {
+      this.next();
+    }, 5000); // Cambia cada 5 segundos
+  }
+
+  stopCarousel(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  next(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prev(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  goTo(index: number): void {
+    this.currentIndex = index;
+    this.stopCarousel();
+    this.startCarousel();
+  }
+}
--- back-office-web/src/main/web/src/app/features/pages/home/home.module.ts
+++ back-office-web/src/main/web/src/app/features/pages/home/home.module.ts
@@ -3,13 +3,15 @@ import {NgModule} from '@angular/core';
 import {HomeComponent} from "./home.component";
 import {MatCardModule} from "@angular/material/card";
 import {CommonModule} from "@angular/common";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [
-    CommonModule,
-    MatCardModule,
-  ],
+    imports: [
+        CommonModule,
+        MatCardModule,
+        MatIconModule,
+    ],
   exports: [HomeComponent],
   providers: [],
 })
